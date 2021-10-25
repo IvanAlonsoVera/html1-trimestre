6b6b2dd6155d20764a4845910887929c42a0d4b4
angusyon <angusyon@fake.com>
2021-10-25 07:29:10 +0200

    25/10/21

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+function menu() {
+  let menumont = "<ul><\li><div id=\"principal"\>\<\a href="\../index.html"\>\<h1\>Principal</h1></a>
+  document.getElementById("menu").src = cambia_imagen[rand];
+}
